d05fdfe7c5b9784649417aba7b73c6ac386dd7d8
rafal <rafalohaki>
2025-08-22 01:04:48 +0200

    upload

---
+++ src/main/java/org/rafalohaki/portalsPermission/PortalsPermission.java
@@ -0,0 +1,17 @@
+package org.rafalohaki.portalsPermission;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PortalsPermission extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
